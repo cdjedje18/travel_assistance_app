ebb2a4f9d92777900c8412aecbc5f05992f9dc0e
Carlos Djedje <cdjedje18@gmail.com>
2023-08-02 21:46:57 +0200

    adding request to openweatherapi

---
+++ back-end/common/config.js
@@ -0,0 +1,8 @@
+let config = {
+    weatherConfigs: {
+        apiUrl: "https://api.openweathermap.org/data/2.5/forecast",
+        apiKey: "702bcddbb96297c82ae7e42d57bd975f"
+    }
+}
+
+module.exports = config
--- back-end/controllers/weatherController.js
+++ back-end/controllers/weatherController.js
@@ -1,7 +1,22 @@
+const request = require('request')
+const config = require('../common/config')
+
+
 const weatherController = {
     index: async (req, res) => {
 
-        res.status(200).send({ weather: null })
+        let city = req.query?.city
+
+        if (city) {
+            let url = `${config.weatherConfigs.apiUrl}?q=${city}&appid=${config.weatherConfigs.apiKey}&cnt=2`
+
+            request.get(url, (error, response, body) => {
+                res.status(200).send({ weather: JSON.parse(body) })
+            })
+
+        } else {
+            res.status(400).send({ message: "City name not provided" })
+        }
 
     }
 
